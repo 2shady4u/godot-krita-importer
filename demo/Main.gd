9e8b7d6aae48492947dda8a2ed66467a6ad30c23
Tim Krief <tim@timkrief.com>
2024-09-09 11:20:10 +0200

    Add import as files option

--- demo/Main.gd
+++ demo/Main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+const EXAMPLE_PATH := "res://addons/godot-krita-importer/examples/example.kra"
+
 func _ready():
 	var importer := KraImporter.new()
-	importer.load("res://addons/godot-krita-importer/examples/example.kra")
+	importer.load(EXAMPLE_PATH)
 
 	var options := {
 		"ignore_invisible_layers": false,
@@ -12,16 +14,17 @@ func _ready():
 	# We can't instance this class, but we can still access the static methods!
 	var import_plugin = preload("res://addons/godot-krita-importer/krita_import_plugin.gd")
 
+	var textures_dir: String =  EXAMPLE_PATH.get_basename() + "_kra_imported_files"
 	for i in range(importer.layer_count - 1, -1, -1):
 		var layer_data : Dictionary = importer.get_layer_data_at(i)
 
 		match(layer_data.get("type", -1)):
 			0:
-				var sprite : Sprite2D = import_plugin.import_paint_layer(layer_data, options)
+				var sprite : Sprite2D = import_plugin.import_paint_layer(layer_data, options, textures_dir)
 				if sprite != null:
 					add_child(sprite)
 			1:
-				var child_node : Node2D = import_plugin.import_group_layer(importer, layer_data, options)
+				var child_node : Node2D = import_plugin.import_group_layer(importer, layer_data, options, textures_dir)
 				if child_node != null:
 					add_child(child_node)
 
