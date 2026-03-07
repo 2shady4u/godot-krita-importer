11e7669e6162f1660889467749b00dc7c135bef8
Chris Herborth <chrish@taffer.ca>
2026-03-07 10:32:26 -0500

    remove get_name() function

--- demo/addons/godot-krita-importer/godot-krita-importer.gd
+++ demo/addons/godot-krita-importer/godot-krita-importer.gd
@@ -9,9 +9,6 @@ extends EditorPlugin
 
 var import_plugin = null
 
-func get_name() -> StringName:
-	return "Godot Krita Importer"
-
 func _enter_tree():
 	import_plugin = preload("krita_import_plugin.gd").new()
 	add_import_plugin(import_plugin)
